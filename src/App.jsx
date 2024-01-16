5bc0df337c70bfde53ddb3feca889219797df362
Akshat Kumar <akkumar@deqode.com>
2024-01-16 23:07:07 +0530

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <h1>Hi from Akshat</h1>
+      <h1>Hi from Akshat!!!!!!</h1>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
